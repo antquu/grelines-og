bee1ca4079924b951d3834e826032e71ac4ff013
antquu <ant.adam468@gmail.com>
2026-08-12 22:07:00 +0200

    OG

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
+
+console.log(`_
+  __ _ _ __ | |_ __ _ _ _
+ / _\` | '_ \\| __/ _\` | | | |
+| (_| | | | | || (_| | |_| |
+ \\__,_|_| |_|\\__\\__, |\\__,_|
+                   |_| 
+                   
+       made by antqu • github.com/antquu`
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
